7f8a7d8d8e536c8778abfe12899128cdfe580b80
Rediet Tilahun Desta <redtil10@gmail.com>
2017-01-07 12:54:15 -0500

    initial setup...can receive and display pictures

---
+++ ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by Rediet Tilahun Desta on 12/28/16.
+//  Copyright © 2016 Rediet Tilahun Desta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    var arrayPosts = [String]()
+    var arrayLinks = [String]()
+    var arrayConditions = [String]()
+    var arrayIDs = [String]()
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "http://localhost:3000/get-data")!
+        
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                print(error!.localizedDescription)
+                
+            } else {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!,options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                        //Implement your logic
+                    print("Begin of code")
+                    let urls = json?["urls"] as! [AnyObject]
+                    for urlObj in urls{
+                        let url = urlObj["url"] as! String
+                    
+                        if let checkedUrl = URL(string: url) {
+                            self.imageView.contentMode = .scaleAspectFit
+                            self.downloadImage(url: checkedUrl)
+                        }
+                    }
+                        print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+                        
+                    
+                    
+                } catch {
+                    
+                    print("error in JSONSerialization")
+                    
+                }
+                
+                
+            }
+            
+        })
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
