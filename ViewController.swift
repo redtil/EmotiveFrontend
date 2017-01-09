2e7851c9462d94a9814e91b1ac9da5f63bfbc571
Rediet Tilahun Desta <redtil10@gmail.com>
2017-01-08 23:24:30 -0500

    able to receive and swipe through pictures from backend

--- ViewController.swift
+++ ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     var arrayLinks = [String]()
     var arrayConditions = [String]()
     var arrayIDs = [String]()
+    var count = 0
     
     func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
         URLSession.shared.dataTask(with: url) {
@@ -47,10 +48,108 @@ class ViewController: UIViewController {
         }
     }
     
+    func respond(gesture: UISwipeGestureRecognizer){
+//        let config = URLSessionConfiguration.default // Session Configuration
+//        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: "http://localhost:3000/get-data")!
+        print("I am in RESPOND")
+        
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data,options: JSONSerialization.ReadingOptions.allowFragments) as? NSDictionary
+                //Implement your logic
+                
+                print("I am in RESPOND2")
+                let urls = json?["urls"] as! [AnyObject]
+                var cnt = 0
+                for urlObj in urls{
+                    let url = urlObj["url"] as! String
+                    
+                    if let checkedUrl = URL(string: url) {
+                        print("I am in RESPOND3")
+                        print(cnt)
+                        print(self.count)
+                        if cnt == self.count{
+                            self.count = self.count + 1
+                            self.imageView.contentMode = .scaleAspectFit
+                            self.downloadImage(url: checkedUrl)
+                            break
+                        }
+                    }
+                    cnt = cnt + 1
+                }
+                print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+                
+                
+                
+            } catch {
+                
+                print("error in JSONSerialization")
+                
+            }
+        }
+        
+        
+//        let task = session.dataTask(with: url, completionHandler: {
+//            (data, response, error) in
+//        
+//            if error != nil {
+//                
+//                print(error!.localizedDescription)
+//                
+//            } else {
+//                do {
+//                    let json = try JSONSerialization.jsonObject(with: data!,options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+//                    //Implement your logic
+//                    
+//                    print("I am in RESPOND2")
+//                    let urls = json?["urls"] as! [AnyObject]
+//                    var cnt = 0
+//                    for urlObj in urls{
+//                        let url = urlObj["url"] as! String
+//                        
+//                        if let checkedUrl = URL(string: url) {
+//                            if cnt == self.count{
+//                                self.count = self.count + 1
+//                                self.imageView.contentMode = .scaleAspectFit
+//                                self.downloadImage(url: checkedUrl)
+//                            }
+//                        }
+//                        cnt = cnt + 1
+//                    }
+//                    print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
+//                    
+//                    
+//                    
+//                } catch {
+//                    
+//                    print("error in JSONSerialization")
+//                    
+//                }
+//                
+//                
+//            }
+//            
+//        })
+//        task.resume()
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         checkLogin()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action:#selector(respond))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action:#selector(respond))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        view.addGestureRecognizer(swipeLeft)
+        
         
         let config = URLSessionConfiguration.default // Session Configuration
         let session = URLSession(configuration: config) // Load configuration into Session
@@ -72,8 +171,13 @@ class ViewController: UIViewController {
                         let url = urlObj["url"] as! String
                     
                         if let checkedUrl = URL(string: url) {
-                            self.imageView.contentMode = .scaleAspectFit
-                            self.downloadImage(url: checkedUrl)
+                            if self.count == 0 {
+                                self.count = self.count + 1
+                                self.imageView.contentMode = .scaleAspectFit
+                                self.downloadImage(url: checkedUrl)
+                                break
+                            }
+                            
                         }
                     }
                         print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
@@ -92,7 +196,9 @@ class ViewController: UIViewController {
         })
         task.resume()
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
