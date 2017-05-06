26e39b9beea4da2f0cb75fb4357c0c2ac637674a
Rediet Tilahun Desta <redtil10@gmail.com>
2017-05-06 12:22:39 -0400

    now can send captured image to the backend

--- ViewController.swift
+++ ViewController.swift
@@ -21,6 +21,7 @@ UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
     var seconds = 10
     
     
+    
     @IBOutlet var imageDesriber: UITextView!
     
     @IBAction func uploadAlbumButton(_ sender: UIButton) {
@@ -82,17 +83,19 @@ UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
             }.resume()
     }
     
-    func runTimedCode(){
-        startCapturing = true
-    }
-    
     func decrementSecs(){
         print("seconds is: ")
-        print(seconds)
-        if seconds >= 1{
-            seconds = seconds-1
+        print(self.seconds)
+        if self.seconds >= 1 && self.seconds != 5{
+            self.seconds = self.seconds-1
+        }
+        else if self.seconds == 5{
+            self.startCapturing = true
+            self.seconds = self.seconds-1
+
         }
         else{
+            self.sendSessionDataToBackEnd()
             let swipeRight = UISwipeGestureRecognizer(target: self, action:#selector(respond))
             swipeRight.direction = UISwipeGestureRecognizerDirection.right
             
@@ -129,26 +132,26 @@ UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
             
             let queue = DispatchQueue(label:"com.ImageViewer.captureQueue")
             dataOutput.setSampleBufferDelegate(self, queue: queue)
-            print("AV session has begun!")
         }
         
     }
     
     //delegate function called continuously because photo capturing session has begun
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        print("captureOutput called")
         if startCapturing{
-        if let image = self.getImageFromSamplBuffer(buffer: sampleBuffer){
-            print("captureOutput image buffer received")
-            DispatchQueue.main.async{
-                let photoVC = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier:"ImageViewerVC") as! ImageViewerController
-                photoVC.takenPhoto = image
-                print("captureOutput image ready to be displayed")
-                self.startCapturing = false
-                self.present(photoVC, animated:true, completion:nil)
-
-            }
-        }
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.capturedPhoto = self.getImageFromSamplBuffer(buffer: sampleBuffer)
+            self.startCapturing = false
+//            if self.capturedPhoto != nil{
+//                print("captureOutput image buffer received")
+//                DispatchQueue.main.async{
+//                    let photoVC = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier:"ImageViewerVC") as! ImageViewerController
+//                    photoVC.takenPhoto = self.capturedPhoto
+//                    print("captureOutput image ready to be displayed")
+//                    self.startCapturing = false
+//                    self.present(photoVC, animated:true, completion:nil)
+//                }
+//            }
         }
     }
     
@@ -179,27 +182,6 @@ UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
         }
     }
     
-//    //using imageView to take pictures every few seconds: doesn't work
-//    func startCameraCaptureProcess(){
-//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-//            let imagePicker = UIImagePickerController()
-//            imagePicker.delegate = self
-//            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-//            imagePicker.allowsEditing = false
-//            imagePicker.cameraDevice = .rear
-//            self.present(imagePicker,animated: true, completion: nil)
-//            print("I am in startCameraCaptureProcess")
-//        }
-//        
-//        //code to save image
-////        let imageData = UIImageJPEGRepresentation(pickedImage.image!, 0.6)
-////        let compressedJPEGImage = UIImage(data: imageData!)
-////        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
-//    }
-    
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!) {
-//        pickedImage.image = image
-//    }
     
     func downloadImage(url: URL) {
         print("Download Started")
@@ -209,20 +191,84 @@ UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
             print("Download Finished")
             DispatchQueue.main.async() { () -> Void in
                 self.imageView.image = UIImage(data: data)
-                Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.runTimedCode), userInfo: nil, repeats: false)
-                Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decrementSecs), userInfo: nil, repeats: true)
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decrementSecs), userInfo: nil, repeats: true)
             }
         }
+    }
+    
+    func sendSessionDataToBackEnd(){
+        self.timer.invalidate()
+        let urlString = myUrlFirstPart + "/users/processSessionData"
+        let myUrl = URL(string: urlString)!
+        let request = NSMutableURLRequest(url:myUrl);
+        request.httpMethod = "POST";
+        
+        let jsonObject: NSMutableDictionary = NSMutableDictionary()
+        print("seconds: ")
+        print(self.seconds)
+        let timeSpent:Double = Double(10 - self.seconds)
+        print("timeSpent: ")
+        print(timeSpent)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        if appDelegate.capturedPhoto == nil{
+            print("capturedPhoto is actually nil")
+        }
+//        DispatchQueue.main.async{
+//            let photoVC = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier:"ImageViewerVC") as! ImageViewerController
+//            photoVC.takenPhoto = appDelegate.capturedPhoto
+//            print("displaying capturedImage at the end of a session!")
+//            self.present(photoVC, animated:true, completion:nil)
+//            
+//        }
+        
+        
+        let imageData = UIImageJPEGRepresentation(appDelegate.capturedPhoto!, 1)
+        jsonObject.setValue(timeSpent, forKey: "timeSpent")
+        jsonObject.setValue(imageData?.base64EncodedString(), forKey: "imageData")
+
+        
+        let jsonData: NSData
+        
+        do {
+            jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions()) as NSData
+            let jsonString = NSString(data: jsonData as Data, encoding: String.Encoding.utf8.rawValue) as! String
+            print("json string = \(jsonString)")
+            request.addValue("/application/json",forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonData as Data
+        } catch _ {
+            print ("JSON Failure")
+        }
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) in
+            
+            if let data = data {
+                do{
+                    let responseString = NSString(data: data, encoding: 8)
+                    print("responseString = \(responseString)")
+                    
+                } catch let error as NSError {
+                    print("I am in the first error")
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print("I am in the second error")
+                print(error.localizedDescription)
+            }
+            
+        })
+        task.resume()
     }
     
     //respond to swipe right or left
     func respond(gesture: UISwipeGestureRecognizer){
         let urlString = myUrlFirstPart + "/get-data";
         let url = URL(string: urlString)!
+        print("right before sendSessionDataToBackEnd seconds: ")
+        print(self.seconds)
         //before setting seconds back to 300 first calculate the time spent before swipe and send to the backend
-        var timeSpent:Double = Double(10 - seconds)/60.0
-        print(timeSpent)
-        seconds = 10
+        
+        
+        self.seconds = 10
         print("I am in RESPOND to gesture")
         
         getDataFromUrl(url: url) { (data, response, error)  in
